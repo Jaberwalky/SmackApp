c0761c039c674f99cbef9804e0dac04872d122af
Jaberwalky <pauljablonski@me.com>
2018-09-26 10:10:19 +0100

    Add login UI with segue

---
+++ smack/Controller/LoginVC.swift
@@ -0,0 +1,24 @@
+//
+//  LoginVC.swift
+//  smack
+//
+//  Created by Paul Jablonski on 26/09/2018.
+//  Copyright © 2018 Oxido. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+    
+    @IBAction func closePressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+
+}
---
+++ smack/Utilities/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  smack
+//
+//  Created by Paul Jablonski on 26/09/2018.
+//  Copyright © 2018 Oxido. All rights reserved.
+//
+
+import Foundation
+
+// Segues
+
+let TO_LOGIN = "toLogin"
